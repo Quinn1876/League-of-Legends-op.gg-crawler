ea3929061bdae8d5420f77d36eaba633c8d30c66
Quinn1876 <qhodges0442@gmail.com>
2022-02-16 11:44:10 -0500

    frontend

--- frontend.py
+++ frontend.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import crawler as cr
+from PIL import ImageTk, Image
 
 class SummonerDisplay(tk.Frame):
   def __init__(self, master=None):
@@ -11,26 +12,49 @@ class SummonerDisplay(tk.Frame):
 
     self.nameLabel = tk.Label(self, textvariable=self.name)
     self.rankLabel = tk.Label(self, textvariable=self.rank)
+    self.padTop = tk.Frame(self, height=20)
+
+    self.__args = []
+    self.__kwargs = {}
 
   def pack(self, *args, **kwargs):
     tk.Frame.pack(self=self, *args, **kwargs)
+    self.padTop.pack(side=tk.TOP)
     self.nameLabel.pack(side=tk.TOP)
     self.rankLabel.pack(side=tk.TOP)
 
+  def place(self, *args, **kwargs):
+    tk.Frame.place(self=self, *args, **kwargs)
+    self.nameLabel.place(x=10, y=0)
+    self.rankLabel.place(x=10, y=10)
+
   def setSummoner(self, name='', rank=''):
+
     self.name.set(name)
     self.rank.set(rank)
 
 
 
 class Main(tk.Frame):
-  def __init__(self, master=None):
-    master.geometry("800x600")
+  def __init__(self, master=None, width=800, height=600):
+    master.geometry(f"{width}x{height}")
     master.title('League Screener')
-    master.protocol("WM_DELETE_WINDOW", self.cleanup)
+    master.resizable(False, False) # make the window a fixed size
+    master.protocol("WM_DELETE_WINDOW", self.cleanup) # Override default shutdown command to clean up the backend
+    master.iconbitmap('assets/icon.ico')
+    # master.update_idletasks()
+    # master.overrideredirect(1)
+    # master.update_idletasks()
     tk.Frame.__init__(self, master)
     self.pack(fill=tk.BOTH, expand=1)
     self.master = master
+
+    ## Set the Backround
+    img = ImageTk.PhotoImage(Image.open('assets/backdrop.jpg').resize((width, height)))
+    self.background = tk.Label(self, image=img)
+    self.background.image = img
+    self.background.place(x=-1, y=0)
+    self.background.pack(side = "bottom", fill = "both", expand = "yes")
 
     self.summonerContainer = tk.Frame(self)
     self.summoner1 = SummonerDisplay(self.summonerContainer)
@@ -39,24 +63,20 @@ class Main(tk.Frame):
     self.summoner4 = SummonerDisplay(self.summonerContainer)
     self.summoner5 = SummonerDisplay(self.summonerContainer)
 
-    self.summonerContainer.pack(side=tk.TOP, fill=tk.BOTH)
+    self.summonerContainer.place(x = 100, y = 100)
     self.summoner1.pack(side=tk.LEFT)
     self.summoner2.pack(side=tk.LEFT)
     self.summoner3.pack(side=tk.LEFT)
     self.summoner4.pack(side=tk.LEFT)
     self.summoner5.pack(side=tk.LEFT)
 
-    self.controls = tk.Frame(self)
-    self.input = tk.Text(master=self.controls, height=5, width=60)
-    self.button_container = tk.Frame(self.controls)
-    self.paste_button = tk.Button(master=self.button_container, text="Paste", command=self.handlePaste, padx=22)
-    self.search_button = tk.Button(master=self.button_container, text="Search", command=self.handleSearch, padx=19)
+    self.input = tk.Text(master=self, height=5, width=60, bg="#26265A", bd=0, fg="#FFAA00", font=("Times New Roman", 12), insertbackground="#FFAA00")
+    self.paste_button = tk.Button(master=self, text="Paste", command=self.handlePaste, padx=22, bg="#26265A", fg="#FFAA00", font=("Times New Roman", 12))
+    self.search_button = tk.Button(master=self, text="Search", command=self.handleSearch, padx=19, bg="#26265A", fg="#FFAA00", font=("Times New Roman", 12))
 
-    self.controls.pack(side=tk.BOTTOM, pady=50)
-    self.button_container.pack(side=tk.RIGHT)
-    self.input.pack(side=tk.RIGHT, padx=50)
-    self.paste_button.pack(pady=4)
-    self.search_button.pack(pady=4)
+    self.input.place(x = 60, y = height - 30, anchor=tk.SW)
+    self.paste_button.place(x = 580, y = height - 120)
+    self.search_button.place(x = 580, y = height - 80)
 
     # Connection to Crawler
     self.crawler = cr.Crawler()
